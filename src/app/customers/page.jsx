d4d9d9491bbe0a7b546772ec0c38763c08cf5d5c
Ali136384 <haseniali920@gmail.com>
2024-01-27 13:50:55 +0300

    responsive-design

--- src/app/customers/page.jsx
+++ src/app/customers/page.jsx
@@ -1,13 +1,11 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { FaUserEdit } from "react-icons/fa";
-import { CiCircleRemove } from "react-icons/ci";
-import { ImProfile } from "react-icons/im";
 import { getAllCustomers } from "../api/v1/customer";
 import "./main.css";
 import Link from "next/link";
 function page() {
   const [AllCustomers, setAllCustomers] = useState([]);
+
   const dummyData = [
     {
       id: 1,
@@ -99,78 +97,99 @@ function page() {
 
   return (
     <>
-      <div className="tabel ml-[23%] mt-10 flex justify-between mr-[5%] shadow-xl h-[650px] rounded-xl p-7 overflow-y-scroll">
-        <div className="custom-table w-full">
-          <div className="head w-full flex justify-between border-b-2 py-3 text-center text-green-900 font-bold text-[18px] ">
-            <span className="w-[9%]">Name</span>
-            <span className="w-[9%]">Surname</span>
-            <span className="w-[12%]">Total Price</span>
-            <span className="w-[12%]">Paid</span>
-            <span className="w-[13%]">Should pay</span>
-            <span className="w-[15%]">Started At</span>
-            <span className="w-[11%]">End At</span>
-            <span className="w-[9%]">Days left</span>
-            <div className="w-[11%]">
-              <span></span>
-              <span></span>
-            </div>
+      <div className="table-main ml-[25%] mt-5 h-[90vh] shadow-md rounded-[31px] p-4 mr-10 ">
+        <div className="parent-cols grid grid-cols-9 gap-4 ">
+          <div className="name-col overflow-hidden">
+            <p className="mb-5">Name</p>
+            {AllCustomers.map((ele) => {
+              return (
+                <>
+                  <p>{ele.name}</p>
+                </>
+              );
+            })}
           </div>
-          {AllCustomers.map((ele) => {
-            const startedAt = ele.startedAt;
-            const dateObject = new Date(startedAt);
-            const formattedDate = dateObject.toLocaleDateString("en-US", {
-              year: "numeric",
-              month: "2-digit",
-              day: "2-digit",
-            });
-
-            const endsAt = ele.endsAt;
-            const endDateObjext = new Date(endsAt);
-            const formatedEndDate = endDateObjext.toLocaleDateString("en-US", {
-              year: "numeric",
-              month: "2-digit",
-              day: "2-digit",
-            });
-
-            const getDayDifference = (formattedDate, formatedEndDate) => {
-              const startDate = new Date(formattedDate);
-              const endDate = new Date(formatedEndDate);
-
-              const timeDifference = endDate.getTime() - startDate.getTime();
-
-              const dayDifference = timeDifference / (1000 * 60 * 60 * 24);
-
-              return Math.abs(dayDifference);
-            };
-
-            return (
-              <>
-                <Link className="" href={`/customer/${ele.id}`}>
-                  <div
-                    key={ele.id}
-                    className="body-table p-2 parent-table-div w-full flex justify-between pt-8 border-b-2 pb-8 text-center hover:bg-customRed bg-opacity-60 hover:text-white duration-300"
-                  >
-                    <span className=" w-[11%] text-start pl-2 ">
-                      {ele.name.length > 8
-                        ? ele.name.slice(0, 8) + "..."
-                        : ele.name}
-                    </span>
-                    <span className=" w-[11%] ">{ele.surname}</span>
-                    <span className=" w-[11%]">{ele.bucketPrice}</span>
-                    <span className=" w-[11%]">{ele.paymentAmount}</span>
-                    <span className=" w-[11%]">
-                      {ele.bucketPrice - ele.paymentAmount}
-                    </span>
-                    <span className=" w-[11%]">{formattedDate}</span>
-                    <span className=" w-[11%]">{formatedEndDate}</span>
-                    <span className=" w-[11%]">
-                      {getDayDifference(formattedDate, formatedEndDate)} days
-                    </span>
-                  </div>{" "}
-                </Link>
-              </>
-            );
-          })}
+          <div className="surname-col overflow-hidden">
+            <p className="mb-5">surname</p>
+            {AllCustomers.map((ele) => {
+              return (
+                <>
+                  <p>{ele.surname}</p>
+                </>
+              );
+            })}
+          </div>
+          <div className="age-col overflow-hidden">
+            <p className="mb-5">age</p>
+            {AllCustomers.map((ele) => {
+              return (
+                <>
+                  <p>{ele.age}</p>
+                </>
+              );
+            })}
+          </div>
+          <div className="bucketPrice-col overflow-hidden">
+            <p className="mb-5">price</p>
+            {AllCustomers.map((ele) => {
+              return (
+                <>
+                  <p>{ele.bucketPrice}</p>
+                </>
+              );
+            })}
+          </div>
+          <div className="paid-col overflow-hidden">
+            <p className="mb-5">Paid</p>
+            {AllCustomers.map((ele) => {
+              return (
+                <>
+                  <p>{ele.paymentAmount}</p>
+                </>
+              );
+            })}
+          </div>
+          <div className="should-pay-col overflow-hidden">
+            <p className="mb-5">Should pay</p>
+            {AllCustomers.map((ele) => {
+              return (
+                <>
+                  <p>{ele.bucketPrice - ele.paymentAmount}</p>
+                </>
+              );
+            })}
+          </div>
+          <div className="started-at-col overflow-hidden">
+            <p className="mb-5">Started At</p>
+            {AllCustomers.map((ele) => {
+              return (
+                <>
+                  <p className=" text-center">Date</p>
+                </>
+              );
+            })}
+          </div>
+          <div className="ends-at-col overflow-hidden">
+            {" "}
+            <p className="mb-5">Ends At</p>
+            {AllCustomers.map((ele) => {
+              return (
+                <>
+                  <p>Date</p>
+                </>
+              );
+            })}
+          </div>
+          <div className="days-left-col overflow-hidden">
+            <p className="mb-5">Days left</p>
+            {AllCustomers.map((ele) => {
+              return (
+                <>
+                  <p>14</p>
+                </>
+              );
+            })}
+          </div>
         </div>
       </div>
     </>
