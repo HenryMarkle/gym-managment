aff56fa907d7a9132b7e3306302c233ecca44999
Ali136384 <haseniali920@gmail.com>
2024-02-08 19:24:16 +0300

    edits

--- src/components/website/Plans.jsx
+++ src/components/website/Plans.jsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import { BallTriangle } from "react-loader-spinner";
 import img from "./website-images/gym-image.png";
 import Image from "next/image";
 
@@ -36,34 +37,49 @@ function Plans() {
           voluptates nihil deleniti nam, veniam, reiciendis quae facilis animi.
           Fugiat, illo.
         </p>
-        <div className="plans w-full flex flex-col gap-14 items-start justify-center mt-16 mx-auto my-0 px-[150px]">
-          {plans.map((ele) => {
-            return (
-              <>
-                <div className="plan flex flex-row gap-4 w-full">
-                  <Image src={img} />
-                  <div className="content-of-plan flex flex-col justify-between w-full">
-                    <p className="text-xl font-medium">{ele.title}</p>
-                    <p
-                      style={{ overflowWrap: "anywhere" }}
-                      className=" opacity-70 py-2"
-                    >
-                      {ele.description}
-                    </p>
-                    <div className="flex justify-between w-full">
-                      <button className=" self-start text-website2 font-medium ">
-                        Register Now
-                      </button>
-                      <span className="text-website2 font-bold">
-                        {ele.price} TL
-                      </span>
+        {plans.length ? (
+          <div className="plans w-full flex flex-col gap-14 items-start justify-center mt-16 mx-auto my-0 px-[150px]">
+            {plans.map((ele) => {
+              return (
+                <>
+                  <div className="plan flex flex-row gap-4 w-full">
+                    <Image src={img} />
+                    <div className="content-of-plan flex flex-col justify-between w-full">
+                      <p className="text-xl font-medium">{ele.title}</p>
+                      <p
+                        style={{ overflowWrap: "anywhere" }}
+                        className=" opacity-70 py-2"
+                      >
+                        {ele.description}
+                      </p>
+                      <div className="flex justify-between w-full">
+                        <button className=" self-start text-website2 font-medium ">
+                          Register Now
+                        </button>
+                        <span className="text-website2 font-bold">
+                          {ele.price} TL
+                        </span>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </>
-            );
-          })}
-        </div>
+                </>
+              );
+            })}
+          </div>
+        ) : (
+          <div className="flex justify-center items-center my-20">
+            <BallTriangle
+              height={100}
+              width={100}
+              radius={5}
+              color="#ed563b"
+              ariaLabel="ball-triangle-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+            />
+          </div>
+        )}
       </div>
     </>
   );
--- src/components/website/Products.jsx
+++ src/components/website/Products.jsx
@@ -2,6 +2,8 @@
 import React, { useEffect, useState } from "react";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { BallTriangle } from "react-loader-spinner";
+
 import { getHomeProducts } from "../../app/api/v1/dashboard";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -18,51 +20,72 @@ function Products() {
   return (
     <>
       <div className="container-site">
-        <Swiper
-          // install Swiper modules
-          modules={[Navigation, Pagination, Scrollbar, A11y]}
-          spaceBetween={50}
-          slidesPerView={3}
-          navigation
-          pagination={{ clickable: true }}
-          scrollbar={{ draggable: true }}
-          // onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log("slide change")}
-        >
-          {" "}
-          {products.map((ele) => {
-            return (
-              <>
-                <SwiperSlide
-                  key={ele.id}
-                  className="product shadow-lg flex flex-col justify-center items-center rounded-xl min-w-[300px]"
-                >
-                  <img
-                    className="w-[220px]"
-                    src="https://cdn.akakce.com/hardline-nutrition/hardline-nutrition-progainer-5000-gr-z.jpg"
-                    alt=""
-                  />
-                  <div className="flex w-full p-3 items-center">
-                    <p className="w-[90%]">
-                      <span className=" text-website2 font-bold mr-1">
-                        {ele.marka}
-                      </span>{" "}
-                      {ele.description.length > 70
-                        ? ele.description.slice(0, 70) + "..."
-                        : ele.description}
-                    </p>
-                  </div>
-                  <div className="flex justify-between items-center w-full p-3">
-                    <p className="text-website2">{ele.price}</p>
-                    <button className="buy-button shadow-md p-2 text-website2 ">
-                      Buy Now
-                    </button>
-                  </div>
-                </SwiperSlide>
-              </>
-            );
-          })}
-        </Swiper>
+        <p className="text-center  font-extrabold text-4xl ">
+          OUR <span className="text-website2">MARKET</span>
+        </p>
+        <p className="waves"></p>
+        <p className="waves"></p>
+        <p className="waves"></p>
+        {products.length ? (
+          <Swiper
+            // install Swiper modules
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={50}
+            slidesPerView={3}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            // onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
+          >
+            {" "}
+            {products.map((ele) => {
+              return (
+                <>
+                  <SwiperSlide
+                    key={ele.id}
+                    className="product shadow-lg flex flex-col justify-center items-center rounded-xl min-w-[300px]"
+                  >
+                    <img
+                      className="w-[220px]"
+                      src="https://cdn.akakce.com/hardline-nutrition/hardline-nutrition-progainer-5000-gr-z.jpg"
+                      alt=""
+                    />
+                    <div className="flex w-full p-3 items-center">
+                      <p className="w-[90%]">
+                        <span className=" text-website2 font-bold mr-1">
+                          {ele.marka}
+                        </span>{" "}
+                        {ele.description.length > 70
+                          ? ele.description.slice(0, 70) + "..."
+                          : ele.description}
+                      </p>
+                    </div>
+                    <div className="flex justify-between items-center w-full p-3">
+                      <p className="text-website2">{ele.price} TL</p>
+                      <button className="buy-button shadow-md p-2 text-website2 ">
+                        Buy Now
+                      </button>
+                    </div>
+                  </SwiperSlide>
+                </>
+              );
+            })}
+          </Swiper>
+        ) : (
+          <div className="flex justify-center items-center my-20">
+            <BallTriangle
+              height={100}
+              width={100}
+              radius={5}
+              color="#ed563b"
+              ariaLabel="ball-triangle-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+            />
+          </div>
+        )}
       </div>
     </>
   );
