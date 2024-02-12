3354f9fc1751fb4412252e26ef38fd01de8cdafb
Ali136384 <haseniali920@gmail.com>
2024-02-12 16:08:15 +0300

    edits

--- src/app/market/[cat]/page.jsx
+++ src/app/market/[cat]/page.jsx
@@ -6,13 +6,15 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
+import BeatLoader from "react-spinners/BeatLoader";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import Link from "next/link";
+import { CiSearch } from "react-icons/ci";
 
 function page({}) {
   const params = useParams();
-
+  const [showSpinner, setShowSpinner] = useState(false);
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -27,7 +29,41 @@ function page({}) {
 
   return (
     <>
-      <div className="cards flex p-10 gap-5 flex-wrap">
+      <div className="input w-[75%] relative border-2 mx-auto flex flex-row justify-between px-1 py-1 rounded-lg mt-4 ">
+        <div className="1 flex w-[95%]">
+          <div className="3">
+            <CiSearch size={27} className="" />
+          </div>
+          <div className="2 w-full">
+            <input
+              onChange={(e) => {
+                setShowSpinner(true);
+                setTimeout(() => {
+                  setShowSpinner(false);
+                }, 2000);
+              }}
+              type="text"
+              className="pl-2 w-full"
+              placeholder="xxxxxxxx"
+            />
+          </div>
+        </div>
+        <div className="2">
+          <BeatLoader
+            color="orange"
+            loading={showSpinner}
+            size={8}
+            speedMultiplier={1}
+          />
+        </div>
+        {/* <button
+              onClick={() => setSendedFilterValue(filterInputValue)}
+              className=" text-white bg-orange-500 px-3 py-[5.3px] rounded-r-md "
+            >
+              Apply
+            </button> */}
+      </div>
+      <div className="cards flex p-10 gap-8 flex-wrap">
         {products.map((ele) => {
           return (
             <>
