ab46fe5cc25a18d00f20e9435c6404a3fa0a914f
Ali136384 <haseniali920@gmail.com>
2024-02-12 14:10:17 +0300

    edtts

--- src/app/market/[cat]/page.jsx
+++ src/app/market/[cat]/page.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+"use client";
+import { useParams } from "next/navigation";
+import { useEffect } from "react";
 
-function page() {
+function page({}) {
+  const params = useParams();
+  useEffect(() => {
+    console.log(
+      "the category you should bring the data by is" + " " + params.cat
+    );
+  });
   return <div>page</div>;
 }
 
