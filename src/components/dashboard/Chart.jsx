e57af2cc5c9cca51199b29b80e089549719b74c9
Ali136384 <haseniali920@gmail.com>
2024-02-15 23:02:33 +0300

    edits

--- src/components/dashboard/Chart.jsx
+++ src/components/dashboard/Chart.jsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
-
 import Chart from "chart.js/auto";
 function ChartComponent({ title, data }) {
 
