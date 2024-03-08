8e18fd1878c951b398294ed7ed792e01491dd7eb
HenryMarkle <mohammad96alsalloum@outlook.sa>
2024-03-09 01:51:17 +0300

    minor changes

--- src/app/api/v1/firebase.ts
+++ src/app/api/v1/firebase.ts
@@ -16,23 +16,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FB_APP_ID,
 };
 
-// const firebaseConfig = {
-
-//   apiKey: "AIzaSyCpDMtQxGtiAelqQSCLbewImcnHKswZvyY",
-
-//   authDomain: "ali-mohamed-gym.firebaseapp.com",
-
-//   projectId: "ali-mohamed-gym",
-
-//   storageBucket: "ali-mohamed-gym.appspot.com",
-
-//   messagingSenderId: "1053790670069",
-
-//   appId: "1:1053790670069:web:cb85a9c432971c6c3d09bc"
-
-// };
-
-
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 
